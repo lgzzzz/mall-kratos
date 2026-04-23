29b32ced8f605e0cd7bcbd938324d1500c171562
lgzzzz <lgzzzz@foxmail.com>
2026-04-23 19:38:41 +0800

    feat(gateway): add gRPC clients with etcd service discovery

--- gateway/go.mod
+++ gateway/go.mod
@@ -3,19 +3,33 @@ module gateway
 go 1.25.0
 
 require (
+	github.com/go-kratos/kratos/contrib/registry/etcd/v2 v2.0.0-20260404020628-f149714c1d54
 	github.com/go-kratos/kratos/v2 v2.9.2
 	github.com/golang-jwt/jwt/v5 v5.3.1
+	github.com/google/wire v0.6.0
+	go.etcd.io/etcd/client/v3 v3.6.10
 	google.golang.org/grpc v1.80.0
 	google.golang.org/protobuf v1.36.11
 )
 
 require (
+	github.com/coreos/go-semver v0.3.1 // indirect
+	github.com/coreos/go-systemd/v22 v22.5.0 // indirect
 	github.com/go-kratos/aegis v0.2.0 // indirect
 	github.com/go-playground/form/v4 v4.2.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
-	github.com/kr/text v0.2.0 // indirect
-	golang.org/x/sys v0.40.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.26.3 // indirect
+	go.etcd.io/etcd/api/v3 v3.6.10 // indirect
+	go.etcd.io/etcd/client/pkg/v3 v3.6.10 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
+	golang.org/x/net v0.51.0 // indirect
+	golang.org/x/sys v0.41.0 // indirect
+	golang.org/x/text v0.35.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20260120221211-b8f7ae30c516 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20260120221211-b8f7ae30c516 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ gateway/internal/client/clients.go
@@ -0,0 +1,66 @@
+package client
+
+import (
+	"context"
+
+	cartV1 "cart-service/api/cart/v1"
+	invV1 "inventory-service/api/inventory/v1"
+	orderV1 "order-service/api/proto/order/v1"
+	paymentV1 "payment-service/api/payment/v1"
+	productV1 "product-service/api/product/v1"
+	promoV1 "promotion-service/api/promotion/v1"
+	userV1 "user-service/api/user/v1"
+
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/registry"
+	kratosgrpc "github.com/go-kratos/kratos/v2/transport/grpc"
+	"github.com/go-kratos/kratos/contrib/registry/etcd/v2"
+	clientv3 "go.etcd.io/etcd/client/v3"
+	"github.com/google/wire"
+	"google.golang.org/grpc"
+)
+
+type Clients struct {
+	User      userV1.UserServiceClient
+	Product   productV1.ProductServiceClient
+	Order     orderV1.OrderServiceClient
+	Cart      cartV1.CartServiceClient
+	Payment   paymentV1.PaymentServiceClient
+	Inventory invV1.InventoryClient
+	Promotion promoV1.PromotionServiceClient
+}
+
+func NewClients(etcdEndpoints []string, logger log.Logger) (*Clients, error) {
+	etcdClient, err := clientv3.New(clientv3.Config{
+		Endpoints: etcdEndpoints,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	discovery := etcd.New(etcdClient)
+
+	return &Clients{
+		User:      userV1.NewUserServiceClient(newGRPCClient(discovery, "discovery:///user-service")),
+		Product:   productV1.NewProductServiceClient(newGRPCClient(discovery, "discovery:///product-service")),
+		Order:     orderV1.NewOrderServiceClient(newGRPCClient(discovery, "discovery:///order-service")),
+		Cart:      cartV1.NewCartServiceClient(newGRPCClient(discovery, "discovery:///cart-service")),
+		Payment:   paymentV1.NewPaymentServiceClient(newGRPCClient(discovery, "discovery:///payment-service")),
+		Inventory: invV1.NewInventoryClient(newGRPCClient(discovery, "discovery:///inventory-service")),
+		Promotion: promoV1.NewPromotionServiceClient(newGRPCClient(discovery, "discovery:///promotion-service")),
+	}, nil
+}
+
+func newGRPCClient(r registry.Discovery, endpoint string) *grpc.ClientConn {
+	conn, err := kratosgrpc.DialInsecure(
+		context.Background(),
+		kratosgrpc.WithEndpoint(endpoint),
+		kratosgrpc.WithDiscovery(r),
+	)
+	if err != nil {
+		panic(err)
+	}
+	return conn
+}
+
+var ProviderSet = wire.NewSet(NewClients)
