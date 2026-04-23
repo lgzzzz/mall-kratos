0795ad96019cd91b066d86f87d959a11c759cda8
lgzzzz <lgzzzz@foxmail.com>
2026-04-23 19:40:20 +0800

    feat(gateway): add gRPC clients with etcd service discovery and recovery middleware

--- gateway/internal/client/clients.go
+++ gateway/internal/client/clients.go
@@ -11,7 +11,7 @@ import (
 	promoV1 "promotion-service/api/promotion/v1"
 	userV1 "user-service/api/user/v1"
 
-	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/middleware/recovery"
 	"github.com/go-kratos/kratos/v2/registry"
 	kratosgrpc "github.com/go-kratos/kratos/v2/transport/grpc"
 	"github.com/go-kratos/kratos/contrib/registry/etcd/v2"
@@ -30,7 +30,7 @@ type Clients struct {
 	Promotion promoV1.PromotionServiceClient
 }
 
-func NewClients(etcdEndpoints []string, logger log.Logger) (*Clients, error) {
+func NewClients(etcdEndpoints []string) (*Clients, error) {
 	etcdClient, err := clientv3.New(clientv3.Config{
 		Endpoints: etcdEndpoints,
 	})
@@ -56,6 +56,9 @@ func newGRPCClient(r registry.Discovery, endpoint string) *grpc.ClientConn {
 		context.Background(),
 		kratosgrpc.WithEndpoint(endpoint),
 		kratosgrpc.WithDiscovery(r),
+		kratosgrpc.WithMiddleware(
+			recovery.Recovery(),
+		),
 	)
 	if err != nil {
 		panic(err)
